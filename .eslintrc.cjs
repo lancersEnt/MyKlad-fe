edcf8a118f3a5464c819ed6d8170359288b15e84
Ghassen Saaf <saafghassen@gmail.com>
2023-03-04 00:08:48 +0100

    initial commit - env setup (vite, react, eslint, prettier, vitest, testing-library/jsdom

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World !</h1>
+      <input
+        type="text"
+        max={5}
+        style={{ color: 'red' }}
+        name="hello_user"
+        className="motherfucker_user"
+      />
+    </div>
+  );
+}
+
+export default App;
